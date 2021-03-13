d4993342a33d1ee61235d0e2b71f618d0b665312
Aniket Rajnish <58925008+aniketrajnish@users.noreply.github.com>
2021-03-13 15:37:53 +0530

    Create Jelly.cs

---
+++ Jelly.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Jelly : MonoBehaviour
+{
+    public float Intensity = 1f;
+    public float Mass = 1f;
+    public float stiffness = 1f;
+    public float damping = 0.75f;
+    private Mesh OriginalMesh, MeshClone;
+    private MeshRenderer renderer;
+    private JellyVertex[] jv;
+    private Vector3[] vertexArray;
+    public bool Sleep = false;
+    
+    public void Start()
+    {
+        OriginalMesh = GetComponent<MeshFilter>().sharedMesh;
+        MeshClone = Instantiate(OriginalMesh);
+        GetComponent<MeshFilter>().sharedMesh = MeshClone;
+        renderer = GetComponent<MeshRenderer>();
+        jv = new JellyVertex[MeshClone.vertices.Length];
+        for (int i = 0; i < MeshClone.vertices.Length; i++)
+            jv[i] = new JellyVertex(i, transform.TransformPoint(MeshClone.vertices[i]));
+    }
+    
+    void FixedUpdate()
+    {
+        vertexArray = OriginalMesh.vertices;
+        if(!Sleep)
+        for (int i = 0; i < jv.Length; i++)
+        {
+            Vector3 target = transform.TransformPoint(vertexArray[jv[i].ID]);
+            float intensity = (1 - (renderer.bounds.max.y - target.y) / renderer.bounds.size.y) * Intensity;
+            jv[i].Shake(target, Mass, stiffness, damping);
+            target = transform.InverseTransformPoint(jv[i].Position);
+            vertexArray[jv[i].ID] = Vector3.Lerp(vertexArray[jv[i].ID], target, intensity);
+        }
+        MeshClone.vertices = vertexArray;
+    }
+    
+    public class JellyVertex
+    {
+        public int ID;
+        public Vector3 Position;
+        public Vector3 velocity, Force;
+        public JellyVertex(int _id, Vector3 _pos)
+        {
+            ID = _id;
+            Position = _pos;
+        }
+        public void Shake(Vector3 target, float m, float s, float d)
+        {
+            Force = (target - Position) * s;
+            velocity = (velocity + Force / m) * d;
+            Position += velocity;
+            if ((velocity + Force + Force / m).magnitude < 0.001f)
+                Position = target;
+        }
+    }
+}
